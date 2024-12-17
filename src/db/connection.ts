63d3e0b68d804b8c4c0c6bd110288160cd86a1db
Daniel Ojo-Williams <ojowilliamsdaniel@gmail.com>
2024-12-17 10:24:42 +0100

    chore: connect to db

---
+++ src/db/connection.ts
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize-typescript";
+import 'dotenv/config';
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  dialect: 'postgres',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  logging: false,
+  port: Number(process.env.DB_PORT),
+  models: [__dirname + "/../**/*.model.ts"]
+});
+
+const connectDB = () => sequelize.authenticate().then(() => sequelize.sync({
+  alter: true
+}))
+
+export default connectDB;
