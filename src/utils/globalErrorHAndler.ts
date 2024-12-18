b5f0c1a3528141bc14603c4421158dbb5f2daea2
Daniel Ojo-Williams <ojowilliamsdaniel@gmail.com>
2024-12-18 18:21:38 +0100

    chore: remove unused serverMessage

--- src/utils/globalErrorHAndler.ts
+++ src/utils/globalErrorHAndler.ts
@@ -6,7 +6,6 @@ import { ZodError } from "zod";
 export const globalErrorHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
   let status = HttpStatus.INTERNAL_SERVER_ERROR;
   let message: string = 'Something went wrong';
-  const serverError = (err as Error).message;
   let data: unknown;
 
   if (err instanceof CustomError) {
